a5d4e445c17e88670952666af30ff2f934108407
Mücahid Rıdvan Kaplan <32431520+MucahidRidvanKaplan@users.noreply.github.com>
2023-06-17 20:00:06 +0300

    Add files via upload

---
+++ SourceCode/timer.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <stdlib.h>
+#include <sys/time.h>
+
+//gettimeofday
+class Timer
+{
+private:
+    double lastTime;
+    //bool on;
+    struct timeb tzp;
+    double getTime()
+    {
+        timeval now;
+        gettimeofday(&now, &tzp);
+        return ((double)now.tv_sec) + ((double)now.tv_usec) / 1000000.;
+    }
+
+public:
+    Timer()
+    {
+        struct timeb tz = {0, 0};
+        tzp = tz;
+        //on = false;
+    }
+
+    void start()
+    {
+        lastTime = getTime();
+    }
+    double stop()
+    {
+        double d = (getTime() - lastTime);
+        return d;
+    }
+    void clear()
+    {
+        lastTime = 0.0;
+    }
+};
